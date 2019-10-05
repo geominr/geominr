c23db8c3c6def55aa1204c2268613b8c8e5f324e
zhikhe <zhikhe@gmail.com>
2019-10-05 12:56:31 -0400

    tooltip and template for world/us toggle

--- assets/js/lab.js
+++ assets/js/lab.js
@@ -1,3 +1,9 @@
+//control scale change
+//todo: switch to vue or react
+const worldDOM = document.querySelector('.nav-link[href="#tab-1-1"]');
+const usDOM = document.querySelector('.nav-link[href="#tab-1-2"]');
+
+
 //census promise
 function censusPromise(args) {
     return new Promise(function (resolve, reject) {
@@ -12,8 +18,6 @@ function censusPromise(args) {
 }
 
 // The svg
-
-
 const svg = d3.select("svg"),
     margin = 10,
     width = +svg.attr("width") - margin,
@@ -57,6 +61,11 @@ function ready(error, us, census_data) {
         .domain(extent)
         .range(d3.schemeBlues[7]);
 
+
+    const tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
     // Draw the map
     svg.append("g")
         .selectAll("path")
@@ -68,7 +77,24 @@ function ready(error, us, census_data) {
             const entry = data.get(d.id);
             return entry ? colorScale(entry.value) : 'rgb(155,155,155)'
         })
-
+        .on("mouseover", e => {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", .9);
+            tooltip.html(() => {
+                    const entry = data.get(e.id);
+                    const value = entry ? entry.value : 'N/A'
+                    return `<strong>${e.properties.name}</strong>: ${value}`;
+                })
+                .style("left", (d3.event.pageX) + "px")
+                .style("top", (d3.event.pageY - 28) + "px");
+            //todo add tip
+        })
+        .on("mouseout", () => {
+            tooltip.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
     // Add legend
     svg.append("g")
@@ -79,7 +105,4 @@ function ready(error, us, census_data) {
             width: 260,
             tickFormat: ".2f"
         }));
-
-    console.log(data.title)
-
 }
